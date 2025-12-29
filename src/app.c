328fbe2b62dba4615aeac17905a0e896c2837952
Azuch <namlucius@gmail.com>
2025-12-29 14:33:27 +0700

    WIP: Adding to app.c wifi init and wifi step overhead

--- src/app.c
+++ src/app.c
@@ -1,6 +1,7 @@
 #include "app.h"
 #include "system_event.h"
 #include "sensor_fsm.h"
+#include "wifi_fsm.h"
 #include <zephyr/logging/log.h>
 #include <zephyr/drivers/watchdog.h>
 #include <zephyr/device.h>
@@ -46,13 +47,20 @@ void watchdog_kick(void) {
 void app_run(void) {
 	g_app.count++;
 	LOG_INF("Started: %d", g_app.count);
+
 	sensor_fsm_init();
+	wifi_fsm_init();
+
 	system_event_post(EVENT_START);
 	enum system_event evt;
 	watchdog_init();
 	while(1) {
+
 		sensor_fsm_step();
+		wifi_fsm_step();
+
 		system_event_get(&evt);
+		
 		switch(evt) {
 			case EVENT_START:
 				LOG_INF("Event start catched");
